88567b5ec7eb8441e12ad793eaedf6cd09d3c1f6
owenlxj <a1019425598@163.com>
2019-01-09 21:51:49 +0800

    Method

---
+++ src/Method.java
@@ -0,0 +1,12 @@
+
+public interface Method {
+	
+	public double addition(double a,double b);
+	
+	public double subtraction(double a,double b);
+	
+	public double multiplication(double a,double b);
+	
+	public double division(double a,double b);
+
+}
